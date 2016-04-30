5a5fd4a73e8c41f4402aaec15f41c0d970d5f865
Michael Alexander <beefsack@gmail.com>
2016-04-30 16:10:29 +1000

    Changed log functions to be implemented on Log

--- src/log.rs
+++ src/log.rs
@@ -7,20 +7,22 @@ pub struct Log<'a> {
     pub to: Vec<usize>,
 }
 
-pub fn new_public(text: &str) -> Log {
-    Log {
-        text: text,
-        at: time::now(),
-        public: true,
-        to: vec![],
+impl<'a> Log<'a> {
+    pub fn public(text: &str) -> Log {
+        Log {
+            text: text,
+            at: time::now(),
+            public: true,
+            to: vec![],
+        }
     }
-}
 
-pub fn new_private(text: &str, to: Vec<usize>) -> Log {
-    Log {
-        text: text,
-        at: time::now(),
-        public: false,
-        to: to,
+    pub fn private(text: &str, to: Vec<usize>) -> Log {
+        Log {
+            text: text,
+            at: time::now(),
+            public: false,
+            to: to,
+        }
     }
 }
