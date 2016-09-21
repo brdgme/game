e76d5e55c89302ec9e1817e0cdc01a223da31e9d
Michael Alexander <beefsack@gmail.com>
2016-09-21 21:24:25 +1000

    Implemented string matcher

--- src/parser.rs
+++ src/parser.rs
@@ -3,11 +3,27 @@ use combine::primitives::Error;
 
 use std::ascii::AsciiExt;
 use std::fmt::{Display, Write};
+use std::collections::HashMap;
 
 use error::GameError;
 
 pub fn cmp_ignore_case(l: char, r: char) -> bool {
     l.eq_ignore_ascii_case(&r)
+}
+
+pub fn match_first<'a, T>(needle: &str,
+                          haystack: &'a HashMap<&str, T>)
+                          -> Result<&'a T, GameError> {
+    let lower_needle = needle.to_lowercase();
+    let matching = || {
+        haystack.iter()
+            .filter(|&(key, _)| key.to_lowercase().starts_with(&lower_needle))
+    };
+    match matching().count() {
+        1 => Ok(matching().nth(0).unwrap().1),
+        0 => Err(GameError::InvalidInput("Couldn't find any matching options".to_string())),
+        _ => Err(GameError::InvalidInput("Ambiguous".to_string())),
+    }
 }
 
 pub fn to_game_error<S>(err: ParseError<S>) -> GameError
@@ -64,4 +80,24 @@ pub fn to_game_error<S>(err: ParseError<S>) -> GameError
     }
 
     GameError::InvalidInput("Invalid input".to_string())
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::collections::HashMap;
+    use ::GameError;
+
+    #[test]
+    fn match_first_works() {
+        let mut hm = HashMap::new();
+        hm.insert("EGGBACON", 1);
+        hm.insert("EGGcheese", 2);
+        assert_eq!(Ok(&1), match_first("eggb", &hm));
+        assert_eq!(Ok(&2), match_first("eggc", &hm));
+        assert_eq!(Err(GameError::InvalidInput("Ambiguous".to_string())),
+                   match_first("egg", &hm));
+        assert_eq!(Err(GameError::InvalidInput("Couldn't find any matching options".to_string())),
+                   match_first("bacon", &hm));
+    }
 }
