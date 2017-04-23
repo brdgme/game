dbed5525f80f50551f1e5927cf7ff7f1b9b2f3ee
Michael Alexander <beefsack@gmail.com>
2017-04-23 14:11:18 +1000

    Made Fuzzer an iterable struct

--- src/bot.rs
+++ src/bot.rs
@@ -5,53 +5,83 @@ use command::Specs as CommandSpecs;
 use errors::*;
 
 pub trait Botter<T: Gamer> {
-    fn commands(player: usize,
+    fn commands(&mut self,
+                player: usize,
                 pub_state: &T::PubState,
                 players: &[String],
                 command_spec: &CommandSpecs)
                 -> Vec<String>;
+}
 
-    fn fuzz(steps: usize) {
-        trace!("Botter::fuzz: starting");
-        let player_counts = T::player_counts();
-        let player_names: Vec<String> = (0..player_counts.iter().max().cloned().unwrap_or(0))
-            .map(|c| format!("{}", c))
-            .collect();
-        let mut rng = rand::thread_rng();
-        let mut step = 0;
-        let mut game_number = 0;
-        'games: loop {
-            game_number += 1;
-            trace!("Botter::fuzz: game {} starting", game_number);
-            let player_count = *rng.choose(&player_counts)
-                                    .expect("game returned no available player counts");
-            let names = &player_names[..player_count];
-            let (mut g, _) = T::new(player_count).expect("game failed to start");
-            assert!(!g.is_finished(),
-                    "game was finished immediately after starting");
-            while !g.is_finished() {
-                let player = *rng.choose(&g.whose_turn())
-                                  .expect("no players in whose_turn");
-                for c in Self::commands(player,
-                                        &g.pub_state(Some(player)),
-                                        names,
-                                        &g.command_spec(player, names)) {
-                    trace!("Botter::fuzz: player {} command '{}'", player, c);
-                    let cmd_res = g.command(player, &c, &[]);
-                    match cmd_res {
-                        Ok(..) => {}
-                        Err(Error(ErrorKind::InvalidInput(_), _)) => {
-                            trace!("Botter::fuzz: invalid input: {}", c)
-                        }
-                        _ => cmd_res.map(|_| ()).unwrap(),
-                    }
+pub struct Fuzzer<G: Gamer, B: Botter<G>> {
+    game: Option<G>,
+    player_counts: Vec<usize>,
+    player_names: Vec<String>,
+    player_count: usize,
+    bot: B,
+    rng: rand::ThreadRng,
+    pub game_count: usize,
+}
+
+impl<G: Gamer, B: Botter<G>> Fuzzer<G, B> {
+    pub fn new(bot: B) -> Self {
+        let player_counts = G::player_counts();
+        Self {
+            game: None,
+            player_names: (0..player_counts.iter().max().cloned().unwrap_or(0))
+                .map(|c| format!("{}", c))
+                .collect(),
+            player_counts: player_counts,
+            player_count: 0,
+            bot: bot,
+            rng: rand::thread_rng(),
+            game_count: 0,
+        }
+    }
+}
+
+impl<G: Gamer, B: Botter<G>> Iterator for Fuzzer<G, B> {
+    type Item = ();
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.game
+               .as_ref()
+               .map(|g| g.is_finished())
+               .unwrap_or(true) {
+            self.game_count += 1;
+            self.player_count = *self.rng
+                                     .choose(&self.player_counts)
+                                     .expect("no player counts for game type");
+            self.game = Some(G::new(self.player_count)
+                                 .expect("failed to create new game")
+                                 .0);
+        } else if let Some(ref mut game) = self.game {
+            let player = *self.rng
+                              .choose(&game.whose_turn())
+                              .expect("is nobody's turn");
+            let pub_state = game.pub_state(Some(player));
+            let command_spec = game.command_spec(player, &self.player_names[..self.player_count]);
+            let input = self.bot
+                .commands(player,
+                          &pub_state,
+                          &self.player_names[..self.player_count],
+                          &command_spec)
+                [0]
+                    .to_owned();
+            let cmd_res = game.command(player, &input, &self.player_names);
+            match cmd_res {
+                Ok(..) => {}
+                Err(Error(ErrorKind::InvalidInput(e), _)) => {
+                    trace!("invalid input '{}' for player {}: {}", input, player, e)
                 }
-                step += 1;
-                if step >= steps {
-                    break 'games;
+                _ => {
+                    panic!("error running command '{}' for player {}, {:?}",
+                           input,
+                           player,
+                           cmd_res)
                 }
             }
         }
-        trace!("Botter::fuzz: completed {} games", game_number);
+        Some(())
     }
 }
